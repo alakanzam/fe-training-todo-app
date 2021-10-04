e335ec5bf4696d98d789b708cc7516146840c14a
Linh Nguyen <alakanzam@hotmail.com>
2021-10-04 21:36:55 +0700

    - Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+
+  userData = [
+    {
+      name: 'Van A',
+      age: 16
+    }, {
+      name: 'Van B',
+      age: 10
+    }
+  ];
+
+
+
+  public appComponentDeleteUser(user): void {
+    console.log('From app component');
+    console.log('Deleted user' + user);
+  }
+
+  public clear() {
+    this.userData = [];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import {PostsListComponent} from './posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
